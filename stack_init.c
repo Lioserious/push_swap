d2dcfdbfecb34fe521833f2204c7fb4c12bf09ae
Leon Ihrig <lihrig@3-H-1.42heilbronn.de>
2025-01-14 16:37:38 +0100

    Version with main that creates linked lists A fills in the input and creates a list that has the same size List b

--- stack_init.c
+++ stack_init.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 15:44:16 by lihrig            #+#    #+#             */
-/*   Updated: 2025/01/13 15:55:20 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/01/14 16:14:23 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	ft_atoi(const char *str)
+{
+	int i;
+	int nbr;
+	int vrz;
+
+	i = 0;
+	nbr = 0;
+	vrz = 1;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			vrz = -1;
+		i++;
+	}
+	if (!(str[i] >= '0' && str[i] <= '9'))
+		return (0);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + (str[i] - '0');
+		i++;
+	}
+	return (nbr * vrz);
+}
 // Erstellt Knoten
 struct Node	*createNode(int value)
 {
@@ -74,38 +101,45 @@ void	printList(struct Node *head)
 	}
 	printf("NULL\n");
 }
-int getListLength(struct Node *head) {
-    int count = 0;
-    struct Node *temp = head;
-    while (temp != NULL) {
-        count++;
-        temp = temp->next;
-    }
-    return count;
+int	getListLength(struct Node *head)
+{
+	int			count;
+	struct Node	*temp;
+
+	count = 0;
+	temp = head;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
 }
 
 int	main(int argc, char **argv)
 {
+	int				i;
+	int				list_length;
+	long long int	value;
+
 	struct Node *listA = NULL; // Erste Liste
 	struct Node *listB = NULL; // Zweite Liste
-
-	// Liste A füllen
-	insertAtEnd(&listA, 10);
-	insertAtEnd(&listA, 20);
-	insertAtEnd(&listA, 30);
-
-	// Liste B füllen
-	insertAtEnd(&listB, 100);
-	insertAtEnd(&listB, 200);
-	insertAtEnd(&listB, 300);
-
-	// Beide Listen anzeigen
-	printf("List A:\n");
+	i = 1;
+	while (i < argc)
+	{
+		value = ft_atoi(argv[i]);
+		insertAtEnd(&listA, value);
+		i++;
+	}
+	list_length = getListLength(listA);
+	i = 0;
+	while (i < list_length)
+	{
+		insertAtEnd(&listB, 0);
+		i++;
+	}
 	printList(listA);
-
-	printf("List B:\n");
+	write(1, "/n", 1);
 	printList(listB);
-
 	return (0);
 }
-
