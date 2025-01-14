f3edec2735aff90d8e461846feeafbf6fc5743d5
Leon Ihrig <lihrig@3-H-1.42heilbronn.de>
2025-01-14 18:00:47 +0100

    added function that pushes first node from b to the beginning of a called pa or push ot a

--- stack_sort.c
+++ stack_sort.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 17:08:46 by lihrig            #+#    #+#             */
-/*   Updated: 2025/01/14 17:17:12 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/01/14 17:56:52 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,20 @@ void sa_swap_a(struct Node **head)
 	temp = (*head)->data;
 	(*head)->data = (*head)->next->data;
     (*head)->next->data = temp;
+}
+//swap a and b at the same time: DER EDGE CASE Koennte probleme machen
+void ss_swap_swap(struct Node **head_a, struct Node **head_b)
+{
+	sa_swap_a(head_a);
+	sa_swap_a(head_b);
+}
+
+void pa(struct Node **head_a, struct Node **head_b)
+{
+	if(*head_b == NULL)
+		return;
+	struct Node *temp = *head_b;
+	*head_b = (*head_b)->next;
+	temp->next = *head_a;
+	*head_a = temp;
 }
