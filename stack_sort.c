21d903e33943d5709a0f6b6590da671dd0532dc7
Leon Ihrig <lihrig@3-H-8.42heilbronn.de>
2025-01-31 16:53:10 +0100

    all sort functions norm conform

--- stack_sort.c
+++ stack_sort.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 17:08:46 by lihrig            #+#    #+#             */
-/*   Updated: 2025/01/31 15:50:42 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/01/31 16:52:09 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,65 +66,4 @@ void	ra_rotate_a(struct Node **head_a)
 	while (curloop->next != NULL)
 		curloop = curloop->next;
 	curloop->next = temp;
-}
-void	rb_rotate_b(struct Node **head_b)
-{
-	struct Node	*temp;
-	struct Node	*curloop;
-
-	if (*head_b == NULL || (*head_b)->next == NULL)
-		return ;
-	temp = *head_b;
-	*head_b = (*head_b)->next;
-	temp->next = NULL;
-	curloop = *head_b;
-	while (curloop->next != NULL)
-		curloop = curloop->next;
-	curloop->next = temp;
-}
-void	rr(struct Node **head_a, struct Node **head_b)
-{
-	ra_rotate_a(head_a);
-	rb_rotate_b(head_b);
-}
-void	rra_reverse_a(struct Node **head_a)
-{
-	struct Node	*prev;
-	struct Node	*curloop;
-
-	if (*head_a == NULL || (*head_a)->next == NULL)
-		return ;
-	prev = NULL;
-	curloop = *head_a;
-	while (curloop->next != NULL)
-	{
-		prev = curloop;
-		curloop = curloop->next;
-	}
-	prev->next = NULL;
-	curloop->next = *head_a;
-	*head_a = curloop;
-}
-void	rrb_reverse_b(struct Node **head_b)
-{
-	struct Node	*prev;
-	struct Node	*curloop;
-
-	if (*head_b == NULL || (*head_b)->next == NULL)
-		return ;
-	prev = NULL;
-	curloop = *head_b;
-	while (curloop->next != NULL)
-	{
-		prev = curloop;
-		curloop = curloop->next;
-	}
-	prev->next = NULL;
-	curloop->next = *head_b;
-	*head_b = curloop;
-}
-void	rrr(struct Node **head_a, struct Node **head_b)
-{
-	rra_reverse_a(head_a);
-	rrb_reverse_b(head_b);
 }
