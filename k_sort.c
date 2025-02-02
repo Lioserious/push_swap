5d0e83c3882c88f0a78d897acb4bbc8ea8526680
Leon Ihrig <lihrig@3-H-8.42heilbronn.de>
2025-02-02 17:17:20 +0100

    sort two function implemented

--- k_sort.c
+++ k_sort.c
@@ -6,22 +6,27 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 16:34:42 by lihrig            #+#    #+#             */
-/*   Updated: 2025/02/02 17:10:32 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/02/02 17:13:22 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int is_sorted(struct s_Node *head)
+int	is_sorted(struct s_Node *head)
 {
-	struct s_Node *current;
+	struct s_Node	*current;
+
 	current = head;
-	
-	while(current != NULL && current->next != NULL)
+	while (current != NULL && current->next != NULL)
 	{
-		if(current->data > current->next->data)
+		if (current->data > current->next->data)
 			return (0);
-		current = current ->next;
+		current = current->next;
 	}
 	return (1);
+}
+void sort_two(struct s_Node **head)
+{
+    if ((*head)->data > (*head)->next->data)
+        sa_swap_a(head);
 }
