99196a03e004c1f870ffd061542055bcd251de35
Leon Ihrig <lihrig@3-G-9.42heilbronn.de>
2025-02-03 16:53:25 +0100

    k sort wasnt pushing back in the proper order needed to adjust it

--- k_sort.c
+++ k_sort.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 16:34:42 by lihrig            #+#    #+#             */
-/*   Updated: 2025/02/03 14:59:13 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/02/03 16:50:12 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,9 @@ void	move_bucket_to_b(struct s_Node **head_a, struct s_Node **head_b,
 		{
 			move_to_position(head_a, position);
 			pb_push_b(head_a, head_b);
+			if (*head_b && (*head_b)->next
+				&& (*head_b)->data < (*head_b)->next->data)
+				sa_swap_a(head_b);
 		}
 		else
 		{
@@ -37,8 +40,23 @@ void	move_bucket_to_b(struct s_Node **head_a, struct s_Node **head_b,
 
 void	move_all_b_to_a(struct s_Node **head_a, struct s_Node **head_b)
 {
+	int	max_pos;
+
 	while (*head_b != NULL)
+	{
+		max_pos = find_max_position(*head_b);
+		if (max_pos <= get_list_length(*head_b) / 2)
+		{
+			while (max_pos-- > 0)
+				rb_rotate_b(head_b);
+		}
+		else
+		{
+			while (max_pos++ < get_list_length(*head_b))
+				rrb_reverse_b(head_b);
+		}
 		pa_push_a(head_a, head_b);
+	}
 }
 
 void	k_sort(struct s_Node **head_a, struct s_Node **head_b)
