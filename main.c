7397d6120cbb93bf347a8c7dc81d8dc433aaab09
Leon Ihrig <lihrig@3-G-9.42heilbronn.de>
2025-02-03 14:51:19 +0100

    main to test code incorporating easy_k_sort

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 15:44:16 by lihrig            #+#    #+#             */
-/*   Updated: 2025/02/02 16:18:43 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/02/03 14:49:36 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	main(int argc, char **argv)
+void easy_k_sort(struct s_Node **head_a, struct s_Node **head_b)
 {
-	struct s_Node	*list_a;
+    int size;
 
-	list_a = NULL;
-	if (argc < 2)
-		return (0);
-	if (process_input(argc, argv, &list_a) != 0)
-		return (1);
-	print_list(list_a);
-	write(1, "\n", 1);
-	free_list(&list_a);
-	return (0);
+    if (*head_a == NULL || is_sorted(*head_a))
+        return;
+
+    size = get_list_length(*head_a);
+    if (size == 2)
+        sort_two(head_a);
+    else if (size == 3)
+        sort_three(head_a);
+    else
+        k_sort(head_a, head_b);
+}
+int main(int argc, char **argv)
+{
+    struct s_Node *list_a;
+    struct s_Node *list_b;
+    
+    list_a = NULL;
+    list_b = NULL;
+    if (argc < 2)
+        return (0);
+    if (process_input(argc, argv, &list_a) != 0)
+        return (1);
+        
+    write(1, "Original list: ", 14);
+    print_list(list_a);
+    write(1, "\n", 1);
+    
+    easy_k_sort(&list_a, &list_b);
+    
+    write(1, "Sorted list:   ", 14);
+    print_list(list_a);
+    write(1, "\n", 1);
+    
+    free_list(&list_a);
+    free_list(&list_b);
+    return (0);
 }
