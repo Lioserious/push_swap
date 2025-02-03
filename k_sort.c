380c1a2b414d9058878707b84b10846ad633f5bb
Leon Ihrig <lihrig@3-G-9.42heilbronn.de>
2025-02-03 15:04:50 +0100

    k_sort splited

--- k_sort.c
+++ k_sort.c
@@ -6,85 +6,11 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 16:34:42 by lihrig            #+#    #+#             */
-/*   Updated: 2025/02/03 14:44:34 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/02/03 14:59:13 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-// 1. Hilfsfunktion: Findet Minimum und Maximum
-void	find_min_max(struct s_Node *head, int *min, int *max)
-{
-	struct s_Node	*current;
-
-	current = head;
-	*min = head->data;
-	*max = head->data;
-	while (current != NULL)
-	{
-		if (current->data < *min)
-			*min = current->data;
-		if (current->data > *max)
-			*max = current->data;
-		current = current->next;
-	}
-}
-
-// 2. Hilfsfunktion: Berechnet die optimale Bucket-Größe
-int	calculate_bucket_size(int min, int max, int list_size)
-{
-	int	range;
-	int	bucket_size;
-
-	// Berechnet die durchschnittliche Größe pro Bucket
-	range = max - min;
-	bucket_size = (range / list_size) + 1;
-	return (bucket_size);
-}
-
-// 3. Hilfsfunktion: Findet die Position der nächsten Zahl im aktuellen Bucket
-int	find_next_in_bucket(struct s_Node *head, int bucket_min, int bucket_max)
-{
-	struct s_Node	*current;
-	int				position;
-
-	current = head;
-	position = 0;
-	while (current != NULL)
-	{
-		if (current->data >= bucket_min && current->data <= bucket_max)
-			return (position);
-		position++;
-		current = current->next;
-	}
-	return (-1); // Keine Zahl im aktuellen Bucket gefunden
-}
-
-// 4. Hilfsfunktion: Optimiert die Bewegung zur Zielposition
-void	move_to_position(struct s_Node **head, int position)
-{
-	int	list_length;
-
-	list_length = get_list_length(*head);
-	// Entscheide ob ra oder rra effizienter ist
-	if (position <= list_length / 2)
-	{
-		// Benutze ra für erste Hälfte
-		while (position > 0)
-		{
-			ra_rotate_a(head);
-			position--;
-		}
-	}
-	else
-	{
-		while (position < list_length)
-		{
-			rra_reverse_a(head);
-			position++;
-		}
-	}
-}
 
 void	move_bucket_to_b(struct s_Node **head_a, struct s_Node **head_b,
 		int current_bucket_min, int bucket_size, int max)
