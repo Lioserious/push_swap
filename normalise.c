ac4d5ac136bd13ca7c07176b25049fe9bf12a276
Leon Ihrig <lihrig@3-G-4.42heilbronn.de>
2025-02-04 16:08:51 +0100

    added header

--- normalise.c
+++ normalise.c
@@ -6,9 +6,11 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 17:46:00 by lihrig            #+#    #+#             */
-/*   Updated: 2025/02/03 17:46:05 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/02/04 16:08:28 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
 
 struct Node* copyList(struct Node* head)
 {
