724025b82dcea923cad0d58142188a3dc47104f6
Leon Ihrig <lihrig@3-G-4.42heilbronn.de>
2025-02-04 17:00:11 +0100

    added bubblesort to sort the cpy

--- normalise.c
+++ normalise.c
@@ -6,35 +6,66 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 17:46:00 by lihrig            #+#    #+#             */
-/*   Updated: 2025/02/04 16:14:36 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/02/04 16:39:58 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-struct s_Node* copyList(struct s_Node* head)
+struct s_Node	*copyList(struct s_Node *head)
 {
-    if (head == NULL)
-        return NULL;
-    
-    struct s_Node* newHead = create_node(head->data);
-    if (newHead == NULL)
-        handle_error(&head);
-    
-    struct s_Node* current = head->next;
-    struct s_Node* newCurrent = newHead;
-    
-    while (current != NULL)
-    {
-        struct s_Node* newNode = create_node(current->data);
-        if (newNode == NULL)
-        {
-            free_list(&newHead);
-            handle_error(&head);
-        }
-        newCurrent->next = newNode;
-        current = current->next;
-        newCurrent = newCurrent->next;
-    }
-    return (newHead);
+	struct s_Node	*newHead;
+	struct s_Node	*current;
+	struct s_Node	*newCurrent;
+	struct s_Node	*newNode;
+
+	if (head == NULL)
+		return (NULL);
+	newHead = create_node(head->data);
+	if (newHead == NULL)
+		handle_error(&head);
+	current = head->next;
+	newCurrent = newHead;
+	while (current != NULL)
+	{
+		newNode = create_node(current->data);
+		if (newNode == NULL)
+		{
+			free_list(&newHead);
+			handle_error(&head);
+		}
+		newCurrent->next = newNode;
+		current = current->next;
+		newCurrent = newCurrent->next;
+	}
+	return (newHead);
 }
+
+void	bubbleSort(struct s_Node *head)
+{
+	struct s_Node	*temp;
+	int				i;
+	int				len;
+	long			s_temp;
+
+	if (head == NULL)
+		return ;
+	len = get_list_length(head);
+	i = 0;
+	while (i < len - 1)
+	{
+		temp = head;
+		while (temp->next != NULL)
+		{
+			if (temp->data > temp->next->data)
+			{
+				s_temp = temp->data;
+				temp->data = temp->next->data;
+				temp->next->data = s_temp;
+			}
+			temp = temp->next;
+		}
+		i++;
+	}
+}
+
